14f7f5765eca5caca028ab89e97d8a6747d1d6f5
om <ombhandwalkar38126@gmail.com>
2025-06-04 19:10:25 +0530

    CodeDeploy

--- Deploy/Scripts/start_docker.sh
+++ Deploy/Scripts/start_docker.sh
@@ -3,10 +3,9 @@
 exec > /home/ubuntu/start_docker.log 2>&1
 
 echo "Logging in to ECR..."
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/b2r6l3l6
-
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/n4j8b0m8
 echo "Pulling Docker image..."
-docker pull public.ecr.aws/b2r6l3l6/yt-chrome-plugin:latest
+docker pull public.ecr.aws/n4j8b0m8/yt-analytics-chat-bot:latest
 
 echo "Checking for existing container..."
 if [ "$(docker ps -q -f name=yt-app)" ]; then
@@ -20,6 +19,6 @@ if [ "$(docker ps -aq -f name=yt-app)" ]; then
 fi
 
 echo "Starting new container..."
-docker run -d -p 80:5000 --name yt-app public.ecr.aws/b2r6l3l6/yt-chrome-plugin:latest
+docker run -d -p 80:5000 --name yt-app public.ecr.aws/n4j8b0m8/yt-analytics-chat-bot:latest
 
 echo "Container started successfully."
